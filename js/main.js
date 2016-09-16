0c3c30354ff8317f7f3045535b8333882b5aaca4
lili hualtibamba <lilihperezz@gmail.com>
2016-09-16 16:20:44 -0500

    agregando cambios

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
+function validateForm(){
+    /* Escribe tú código aquí */
+    var name = document.getElementById("name").value;
+       if (name == ""){
+          alert("llenar el campo nombre por favor");
+       } else if (/^[a-z]/.test(name.charAt(0))){
+          alert("Por favor ingrese la primera letra en mayuscula");
+       } else if (/^[0-9]/.test(name.charAt(0))){
+          alert("Por favor no ingrese numeros");
+    }
 
-window.addEventListener("load", function() {
-    var boton = document.getElementById("iniciar");
-    boton.addEventListener("click", function() {
-         var name = document.getElementById("name").value;
-         var lastname = document.getElementById("lastname").value;
-         var email = document.getElementById("input-email").value;
-         var password = document.getElementById("input-password").value;
-         var menu = document.querySelector("select").value;
-         var expreg = /^[az]$/;
-           
-        validateForm(name,lastname,email,password,menu,expreg);    
-    });
-    
-    function validateForm(name,lastname,email,password,menu,expreg){
-        /* Escribe tú código aquí */
-        if (name == ""){
-            alert("llenar el campo nombre por favor");
-            }
-        if (lastname == ""){
-            alert("llenar el campo apellido por favor");
-            }
-        if (email == ""){
-            alert("llenar el campo correo por favor");
-            }
+    var lastname = document.getElementById("lastname").value;
+        if (lastname == null || lastname.length == 0){
+          alert("llenar el campo apellido por favor");
+        }
+
+    var correovalido=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    var email = document.getElementById("input-email").value;   
+        if (email == "" ){
+           alert("llenar el campo correo por favor");
+        } else if(!correovalidado.test(email)) {
+           alert("Ingrese un correo válido.");
+    }
+
+    var password = document.getElementById("input-password").value;
         if (password == "") {
-            alert("llenar el campo contraseña por favor");
-            }
-        if (menu == 0) {
-            alert("selecciona un tipo de bici por favor");
-            }
-        if (password.length < 6){ 
-            window.alert("Ingrese  al menos 6 caracteres"); 
-            }
-        if (password == "123456" || password == "password" || password == "098754"){
-            alert ("ingresa contraseña valida");
-          }
-     }
- });
+           alert("llenar el campo contraseña por favor");
+        } else if (password.length < 6){ 
+           alert("Ingrese  al menos 6 caracteres"); 
+        } else if (password == "123456" || password == "password" || password == "098754"){
+           alert("ingresa contraseña valida");
+    }
+
+    var menu = document.querySelector("select").value;
+        if (menu == 0) 
+        alert("selecciona un tipo de bici");
+    }
